91d9ba9c9b2c7709e8565229856bdf68552e5955
Cyberattack-del <zahh87476@gmail.com>
2025-06-12 22:42:01 +0530

    ggg

--- utils/getClient.js
+++ utils/getClient.js
@@ -1,9 +1,29 @@
+// function getClientIP(req) {
+//   const ip = req.headers['cf-connecting-ip'] ||
+//              (req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0].trim() : null) ||
+//              req.connection?.remoteAddress ||
+//              req.socket?.remoteAddress ||
+//              req.ip || 'Unknown';
+
+//   return normalizeIP(ip)
+// }
+
+// function normalizeIP(ip) {
+//   if (!ip) return 'Unknown';
+//   if (ip.startsWith('::ffff:')) ip = ip.replace('::ffff:', '');
+//   if (ip === '::1') ip = '127.0.0.1';
+//   return ip;
+// }
+
+// module.exports = { getClientIP };
 function getClientIP(req) {
-  const ip = req.headers['cf-connecting-ip'] ||
-             (req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0].trim() : null) ||
-             req.connection?.remoteAddress ||
-             req.socket?.remoteAddress ||
-             req.ip || 'Unknown';
+  const forwarded = req.headers['x-forwarded-for'];
+  const ip =
+    req.headers['cf-connecting-ip'] || // Cloudflare
+    (forwarded ? forwarded.split(',')[0].trim() : null) ||
+    req.connection?.remoteAddress ||
+    req.socket?.remoteAddress ||
+    req.ip || 'Unknown';
 
   return normalizeIP(ip);
 }
